c9c26999e57599f47b13a9c157ab2289d9d2edde
blofse <blofse@users.noreply.github.com>
2017-05-09 13:55:07 +0100

    Fixing the postgres service, amending initial start to take care of the postgres setup

--- initial_start.sh
+++ initial_start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d --name sonar-alpine-centos7 --link sonar-postgres:pgsonar -p 9000:9000 -e SONARQUBE_JDBC_USERNAME=sonar -e SONARQUBE_JDBC_PASSWORD='(abc123)' -e SONARQUBE_JDBC_URL=jdbc:postgresql://pgsonar:5432/sonar sonar-alpine-centos7
+if [[ $# -eq 0 ]] ; then
+    echo 'Expecting one argument'
+    exit 0
+fi
+
+docker run --name sonar-postgres -e POSTGRES_USER=sonar -e POSTGRES_PASSWORD='$1' -d postgres
+docker run -d --name sonar-alpine-centos7 --link sonar-postgres:pgsonar -p 9000:9000 -e SONARQUBE_JDBC_USERNAME=sonar -e SONARQUBE_JDBC_PASSWORD='$1' -e SONARQUBE_JDBC_URL=jdbc:postgresql://pgsonar:5432/sonar sonar-alpine-centos7
+
