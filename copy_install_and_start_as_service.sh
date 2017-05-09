f2ccc64102a731f7a097daf8eaedf5f13041fca7
blofse <blofse@users.noreply.github.com>
2017-05-09 13:41:40 +0100

    Making sonar a proper service, also adding sonar-postgres service

---
+++ copy_install_and_start_as_service.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo Stopping existing container
+docker stop $(docker ps -a -q  --filter ancestor=postgres)
+docker stop $(docker ps -a -q  --filter ancestor=sonar-alpine-centos7)
+
+echo Copying and running service
+yes | cp docker-sonar-postgres.service /etc/systemd/system/.
+yes | cp docker-sonar.service /etc/systemd/system/.
+systemctl daemon-reload
+
+systemctl start docker-sonar-postgres
+systemctl start docker-sonar
+echo Done!
